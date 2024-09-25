72987c69017809389a1507383c432a9217c421ae
VanMatsuda <>
2024-09-25 10:27:30 -0300

    Resolução do exercicio e comentando atividade

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,30 +1,32 @@
 package main.java;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class App {
-        //Comentário
 
-    public static void funcao_teste (){
-        //Biblioteca para entrada de dados
-        Scanner ler = new Scanner(System.in); 
-
-        //variável local a
-        String v2;
-        int v1;
-
-        System.out.println("Comando de Saida de Dados");
-        
-        System.out.printf("Informe uma palavra: ");
-        v2 = ler.next();
-
-        System.out.printf("Informe um número: ");
-        v1 = ler.nextInt();
-
-        System.out.printf(v2+" "+v1);
-    }
     public static void main(String[] args) throws Exception {
-        funcao_teste();
+        Locale.setDefault(Locale.US);
+        //Biblioteca para entrada de dados
+        Scanner sc = new Scanner(System.in); 
+        
+        System.out.print("Entre com a massa: ");
+        double massa = sc.nextDouble();
 
+        System.out.print("Entre com a altura: ");
+        double altura = sc.nextDouble();
+
+        CorpoHumano cp = new CorpoHumano(massa , altura);
+
+        System.out.println(cp.calcIMC());
+
+        sc.close();
 
     }
 }
+// RESPOSTAS
+/*
+ * 1° A não é possível alterar diretamente  c1.massa = "2"; pois o atributo massa é privado;
+ * 2° Ao alterar o o acesso para oublic é possível fazer alterações diretamente;
+ * 3° O tipo private não permite ter acesso além da própria classe, então retorna erro;
+ * 
+ */
--- src/main/java/CorpoHumano.java
+++ src/main/java/CorpoHumano.java
@@ -1,5 +1,36 @@
 package main.java;
 
 public class CorpoHumano {
+    private double massa;
+    private double altura;
 
+    public CorpoHumano(){
+
+    }
+
+    public CorpoHumano(double massa, double altura) {
+        this.massa = massa;
+        this.altura = altura;
+    }
+
+    public double getMassa() {
+        return massa;
+    }
+
+    public void setMassa(double massa) {
+        this.massa = massa;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    public double calcIMC(){
+        return this.massa / this.altura;
+    }
+    
 }
